36410f1eb05f7664b764cc4acdada997f65c4d7b
csanz91 <cesarsanz.91@gmail.com>
2015-01-27 19:45:30 +0100

    Comentarios

--- I2Cserver.ino
+++ I2Cserver.ino
@@ -1,40 +1,48 @@
 #include <Wire.h>
 
-#define ADDR_SERVO	1
+//Direcciones de los dispositivos conectados al bus I2C
+#define ADDR_SERVER	1
 #define ADDR_LCD	2
-#define NUM_DATA	5
 
+#define NUM_DATA	5						//Num de datos que almacenara el array
 
+//Definimos la posicion de cada dato en el array
 #define data1	0
 #define data2	1
 
 
-int data[NUM_DATA];
+int data[NUM_DATA];							//Array de datos
 int i=0;
 
 void setup() {
 	Serial.begin(9600);
-	Wire.begin(ADDR_SERVO);                // join i2c bus
-	Wire.onRequest(request);
-	inicializar_data();
+	Wire.begin(ADDR_SERVO);                //Nos unimos al bus I2C con la direccion dada
+	Wire.onRequest(request);			   //Definimos la funcion que se ejecuta cuando el master pide los datos
+	inicializar_data();					   //Inicializamos el array de datos para comprobar que los datos de mandan correctamente
 }
 
 void loop() {
-//ola
+
 }
 
 
+//Envia un dato de tipo int cuando lo pide el master. Dentro de esta funcion solo se puede ejecutar un Wire.write()
 void request(){
 
-	char buffer[5];
-	sprintf(buffer, "%05d", data[i]);
-	Wire.write(buffer);
-	i++;
-	if(i>=NUM_DATA) i=0;
+	//Convertimos el array en un string. El array almacenara como maximo 5 digitos mas el signo
+	char buffer[6];    					
+	sprintf(buffer, "%06d", data[i]);		//Generamos el array y rellenamos las posiciones vacias con ceros
+	Wire.write(buffer);						//Envia el array al master
+
+	//Cambia a la siguiente posicion del array para enviar el dato cuando lo vuelva a pedir el master
+	i++;									
+	if(i==NUM_DATA) i=0;
 }
 
+
+//Funcion para inicializar los datos. Valida unicamente para probar que los datos se envian correctamete
 void inicializar_data(){
-	for(int i=0; i<NUM_DATA; i++){
-		data[i]=i;
+	for(int x=0; x<NUM_DATA; x++){
+		data[x]=x;
 	}
 }
