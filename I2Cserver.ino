0b1952d226f588494240d3386983524dec143ac4
csanz91 <cesarsanz.91@gmail.com>
2015-01-27 23:20:03 +0100

    Initial float release

--- I2Cserver.ino
+++ I2Cserver.ino
@@ -10,14 +10,15 @@
 #define data1	0
 #define data2	1
 
+volatile byte* INPUT1FloatPtr;
 
-int data[NUM_DATA];							//Array de datos
+float data[NUM_DATA];							//Array de datos
 int i=0;
 
 void setup() {
 	Serial.begin(9600);
 	Wire.begin(ADDR_SERVER);               //Nos unimos al bus I2C con la direccion dada
-	Wire.onRequest(request);			   //Definimos la funcion que se ejecuta cuando el master pide los datos
+	Wire.onRequest(requestfloat);			   //Definimos la funcion que se ejecuta cuando el master pide los datos
 	inicializar_data();					   //Inicializamos el array de datos para comprobar que los datos de mandan correctamente
 }
 
@@ -26,23 +27,24 @@ void loop() {
 }
 
 
-//Envia un dato de tipo int cuando lo pide el master. Dentro de esta funcion solo se puede ejecutar un Wire.write()
-void request(){
-
-	//Convertimos el array en un string. El array almacenara como maximo 5 digitos mas el signo
-	char buffer[6];    					
-	sprintf(buffer, "%06d", data[i]);		//Generamos el array y rellenamos las posiciones vacias con ceros
-	Wire.write(buffer);						//Envia el array al master
+//Envia un dato de tipo float cuando lo pide el master. Dentro de esta funcion solo se puede ejecutar un Wire.write()
+void requestfloat(){
+	byte DatatoSend[4];						//Vamos a enviar un float, por lo que necesitaremos 4 bytes
+ 	INPUT1FloatPtr = (byte*) &data[i];
+	DatatoSend[0] = INPUT1FloatPtr[0]; 
+	DatatoSend[1] = INPUT1FloatPtr[1]; 
+	DatatoSend[2] = INPUT1FloatPtr[2]; 
+	DatatoSend[3] = INPUT1FloatPtr[3]; 
+	Wire.write(DatatoSend, 4);
 
 	//Cambia a la siguiente posicion del array para enviar el dato cuando lo vuelva a pedir el master
 	i++;									
 	if(i==NUM_DATA) i=0;
 }
 
-
 //Funcion para inicializar los datos. Valida unicamente para probar que los datos se envian correctamete
 void inicializar_data(){
 	for(int x=0; x<NUM_DATA; x++){
-		data[x]=x-2;
+		data[x]=123456.789;
 	}
 }
