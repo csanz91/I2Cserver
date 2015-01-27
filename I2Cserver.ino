facdf672b6625e551c73d7e3518aec3d5c422a02
csanz91 <your email in here>
2015-01-27 14:50:26 +0100

    Initial release

---
+++ I2Cserver.ino
@@ -0,0 +1,40 @@
+#include <Wire.h>
+
+#define ADDR_SERVO	1
+#define ADDR_LCD	2
+#define NUM_DATA	5
+
+
+#define data1	0
+#define data2	1
+
+
+int data[NUM_DATA];
+int i=0;
+
+void setup() {
+	Serial.begin(9600);
+	Wire.begin(ADDR_SERVO);                // join i2c bus
+	Wire.onRequest(request);
+	inicializar_data();
+}
+
+void loop() {
+
+}
+
+
+void request(){
+
+	char buffer[5];
+	sprintf(buffer, "%05d", data[i]);
+	Wire.write(buffer);
+	i++;
+	if(i>=NUM_DATA) i=0;
+}
+
+void inicializar_data(){
+	for(int i=0; i<NUM_DATA; i++){
+		data[i]=i;
+	}
+}
